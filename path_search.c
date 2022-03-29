ac774baf263277fe5817fe961a8a1ebebd6998ca
Grzegorz Jewusiak <grzegorz@jewusiak.pl>
2022-03-29 20:58:10 +0200

    Dodanie funkcji obsługi sciezek (sciezka_t, sciezka_tab)

--- path_search.c
+++ path_search.c
@@ -1,5 +1,23 @@
-//
-// Created by Grzegorz Jewusiak on 29/03/2022.
-//
-
 #include "path_search.h"
+
+
+sciezka_t new_sciezka_t(int id) {
+    return (sciezka_t) { .n=0, .ids=malloc(sizeof (int))};
+}
+
+void add_to_sciezka(sciezka_t *s, int id) {
+    s->ids = realloc(s->ids, (++(s->n)) * sizeof *s->ids);
+    s->ids[s->n - 1]=id;
+}
+
+sciezka_tab new_sciezka_tab(){
+    sciezka_tab t= malloc(sizeof *t);
+    t->n=0;
+    t->sciezki= malloc(sizeof *t->sciezki);
+    return t;
+}
+
+void add_s_to_tab(sciezka_t w, sciezka_tab tab){
+    tab->sciezki= realloc(tab->sciezki, ++(tab->n) * sizeof *tab->sciezki);
+    tab->sciezki[tab->n-1]=w;
+}
--- path_search.h
+++ path_search.h
@@ -14,6 +14,17 @@ typedef struct{
     int n;
 } * sciezka_tab;
 
+//obsługa struktur
+sciezka_t new_sciezka_t(int id);
+
+void add_to_sciezka(sciezka_t *s, int id) ;
+
+sciezka_tab new_sciezka_tab();
+
+void add_s_to_tab(sciezka_t w, sciezka_tab tab);
+
+
+
 sciezka_tab find(wierzcholek_tab tablica);
 
 #endif //JIMP2_GRAPH_PATH_SEARCH_H
